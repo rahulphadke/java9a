a8bf7a64fc850c8a8003491597e38037278c0679
rahulphadke <gotorahul@gmail.com>
2023-08-07 20:03:28 +0530

    after pull and adding src etc

---
+++ src/a1/A.java
@@ -0,0 +1,13 @@
+package a1;
+
+public class A {
+
+	public void f1() {
+		System.out.println("f1()");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hi p1.A main()");
+	}
+
+}
